6f6f8061215e1b7f133f3e157dc2fc6254e96534
guan <1027526351@qq.com>
2017-12-07 21:29:02 +0800

    使用了贼方便的echart插件,表格和饼状图

---
+++ public/manage_my/js/index.js
@@ -0,0 +1,81 @@
+$(function () {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart1 = echarts.init(document.getElementById('main-left'));
+
+    // 指定图表的配置项和数据
+    var option1 = {
+        title: {
+            text: '2017年注册人数'
+        },
+        tooltip: {},
+        legend: {
+            data: ['人数']
+        },
+        xAxis: {
+            data: ["1月", "2月", "3月", "4月", "5月", "6月"]
+        },
+        yAxis: {},
+        series: [{
+            name: '人数',
+            type: 'bar',
+            data: [3000, 2100, 6000, 1400, 4500, 2700]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart1.setOption(option1);
+
+    var myChart2 = echarts.init(document.getElementById('main-right'));
+    option2 = {
+        title: {
+            text: '热门品牌销售',
+            subtext: '2017年6月',
+            x: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['耐克', '阿迪', '百伦', '安踏', '李宁']
+        },
+        series: [{
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [{
+                    value: 335,
+                    name: '耐克'
+                },
+                {
+                    value: 310,
+                    name: '阿迪'
+                },
+                {
+                    value: 234,
+                    name: '百伦'
+                },
+                {
+                    value: 135,
+                    name: '安踏'
+                },
+                {
+                    value: 1548,
+                    name: '李宁'
+                }
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }]
+    };
+    myChart2.setOption(option2);
+
+})
