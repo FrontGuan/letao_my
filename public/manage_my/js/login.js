557c0ddbb1cc9c4b9049058bdaae22a06149dc38
guan <1027526351@qq.com>
2017-12-07 20:40:08 +0800

    忘记创建分支了,先提交一点

--- public/manage_my/js/login.js
+++ public/manage_my/js/login.js
@@ -1,16 +1,4 @@
 $(function () {
-    // $('button[type=submit]').click(function(event){
-    //     // alert(000);
-    //     event.preventDefault();      
-    //     $.ajax({
-    //         url:'/employee/employeeLogin',
-    //         data:$("form").serialize(),
-    //         type: 'post',
-    //         success:function(backData){
-    //             console.log(backData);
-    //         }
-    //     })
-    // })
 
     $('form').bootstrapValidator({
         //2. 指定校验时的图标显示，默认是bootstrap风格
