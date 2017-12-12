3f965109c456754f83c0bccf0f3256ea599d2fbd
guan <1027526351@qq.com>
2017-12-12 13:43:35 +0800

    分类页面布局加样式完成,搜索页面和搜索列表页面布局和样式完成

--- public/mobile_my/js/common.js
+++ public/mobile_my/js/common.js
@@ -1,3 +1,4 @@
 mui('.mui-scroll-wrapper').scroll({
-	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+	deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+	indicators: false
 });
